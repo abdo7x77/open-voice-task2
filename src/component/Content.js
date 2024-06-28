9d86e98c07e8f35f94a0b5e23988f106f05cbd2b
abdo <abdelrhman1562004@gmail.com>
2024-06-28 22:09:08 +0300

    hi

---
+++ src/component/Content.js
@@ -0,0 +1,333 @@
+import './cont.css'
+import Homeimg from './images/home-img.svg'
+export default function Content(){
+
+    return(
+        <div>
+        <header className="header">
+        <a href="#" className="logo"><i className="fas fa-heartbeat"></i>medcare</a>
+        <nav className="navbar">
+          <a href="#home">home</a>
+          <a href="#service">service</a>
+          <a href="#about">about</a>
+          <a href="#doctors">doctors</a>
+          <a href="#book">book</a>
+          <a href="#review">review</a>
+          <a href="#blogs">blogs</a>
+        </nav>
+        <div id="menu-btn" className="fas fa-bars"></div>
+      </header>
+      
+      <section className="home" id="home">
+        <div className="image">
+          <img src="image/home-img.svg" alt="Home" />
+        </div>
+        <div className="content">
+          <h3>stay safe, stay healthy</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corrupti dolorem consequatur! Rem autem ut, porro impedit veniam perspiciatis.</p>
+          <a href="#" className="btn">contact us <span className="fas fa-chevron-right"></span></a>
+        </div>
+      </section>
+
+      <section className="icons-container">
+        <div className="icons">
+          <i className="fas fa-user-md"></i>
+          <h3>140+</h3>
+          <p>doctors at work</p>
+        </div>
+        <div className="icons">
+          <i className="fas fa-users"></i>
+          <h3>1040+</h3>
+          <p>satisfied patients</p>
+        </div>
+        <div className="icons">
+          <i className="fas fa-procedures"></i>
+          <h3>500+</h3>
+          <p>bed facility</p>
+        </div>
+        <div className="icons">
+          <i className="fas fa-hospital"></i>
+          <h3>80+</h3>
+          <p>available hospitals</p>
+        </div>
+      </section>
+
+      <section className="service" id="service">
+        <h1 className="heading">our <span>services</span></h1>
+        <div className="box-container">
+          <div className="box">
+            <i className="fas fa-notes-medical"></i>
+            <h3>free checkups</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, exercitationem?</p>
+            <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+          </div>
+          <div className="box">
+            <i className="fas fa-ambulance"></i>
+            <h3>24/7 ambulance</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, exercitationem?</p>
+            <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+          </div>
+          <div className="box">
+            <i className="fas fa-user-md"></i>
+            <h3>expert doctors</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, exercitationem?</p>
+            <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+          </div>
+          <div className="box">
+            <i className="fas fa-pills"></i>
+            <h3>medicines</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, exercitationem?</p>
+            <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+          </div>
+          <div className="box">
+            <i className="fas fa-procedures"></i>
+            <h3>bed facility</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, exercitationem?</p>
+            <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+          </div>
+          <div className="box">
+            <i className="fas fa-heartbeat"></i>
+            <h3>total care</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, exercitationem?</p>
+            <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+          </div>
+        </div>
+      </section>
+
+      <section className="about" id="about">
+        <h1 className="heading"><span>about</span> us</h1>
+        <div className="row">
+          <div className="image">
+            <img src="image/about-img.svg" alt="About" />
+          </div>
+          <div className="content">
+            <h3>we take care of your healthy life</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, iusto!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis debitis, voluptates accusamus dicta perferendis facere excepturi in quis modi pariatur!</p>
+            <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+          </div>
+        </div>
+      </section>
+
+      <section className="doctors" id="doctors">
+        <div className="box-container">
+          <div className="box">
+            <img src="image/doc-1.jpg" alt="Doctor 1" />
+            <h3>rehab mostafa</h3>
+            <span>expert doctor</span>
+            <div className="share">
+              <a href="#" className="fab fa-facebook-f"></a>
+              <a href="#" className="fab fa-twitter"></a>
+              <a href="#" className="fab fa-instagram"></a>
+              <a href="#" className="fab fa-linkedin"></a>
+            </div>
+          </div>
+          <div className="box">
+            <img src="image/doc-2.jpg" alt="Doctor 2" />
+            <h3>ali esmail</h3>
+            <span>expert doctor</span>
+            <div className="share">
+              <a href="#" className="fab fa-facebook-f"></a>
+              <a href="#" className="fab fa-twitter"></a>
+              <a href="#" className="fab fa-instagram"></a>
+              <a href="#" className="fab fa-linkedin"></a>
+            </div>
+          </div>
+          <div className="box">
+            <img src="image/doc-4.jpg" alt="Doctor 4" />
+            <h3>abdalla abdelsabour</h3>
+            <span>expert doctor</span>
+            <div className="share">
+              <a href="#" className="fab fa-facebook-f"></a>
+              <a href="#" className="fab fa-twitter"></a>
+              <a href="#" className="fab fa-instagram"></a>
+              <a href="#" className="fab fa-linkedin"></a>
+            </div>
+          </div>
+          <div className="box">
+            <img src="image/doc-5.jpg" alt="Doctor 5" />
+            <h3>hossam ali</h3>
+            <span>expert doctor</span>
+            <div className="share">
+              <a href="#" className="fab fa-facebook-f"></a>
+              <a href="#" className="fab fa-twitter"></a>
+              <a href="#" className="fab fa-instagram"></a>
+              <a href="#" className="fab fa-linkedin"></a>
+            </div>
+          </div>
+          <div className="box">
+            <img src="image/doc-6.jpg" alt="Doctor 6" />
+            <h3>raafat omar</h3>
+            <span>expert doctor</span>
+            <div className="share">
+              <a href="#" className="fab fa-facebook-f"></a>
+              <a href="#" className="fab fa-twitter"></a>
+              <a href="#" className="fab fa-instagram"></a>
+              <a href="#" className="fab fa-linkedin"></a>
+            </div>
+          </div>
+          <div className="box">
+            <img src="image/doc-3.jpg" alt="Doctor 3" />
+            <h3>osama saad</h3>
+            <span>expert doctor</span>
+            <div className="share">
+              <a href="#" className="fab fa-facebook-f"></a>
+              <a href="#" className="fab fa-twitter"></a>
+              <a href="#" className="fab fa-instagram"></a>
+              <a href="#" className="fab fa-linkedin"></a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="book" id="book">
+        <h1 className="heading"><span>book</span> now</h1>
+        <div className="row">
+          <div className="image">
+            <img src="image/book-img.svg" alt="Book" />
+          </div>
+          <form>
+            <h3>book appointment</h3>
+            <input type="text" placeholder="your name" className="box" />
+            <input type="number" placeholder="your number" className="box" />
+            <input type="email" placeholder="your email" className="box" />
+            <input type="date" className="box" />
+            <input type="submit" value="book now" className="btn" />
+          </form>
+        </div>
+      </section>
+
+      <section className="review" id="review">
+        <h1 className="heading">client's <span>review</span></h1>
+        <div className="box-container">
+          <div className="box">
+            <img src="image/pic-1.png" alt="Client 1" />
+            <h3>john deo</h3>
+            <span>satisfied client</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, fugiat!</p>
+            <div className="stars">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+          <div className="box">
+            <img src="image/pic-2.png" alt="Client 2" />
+            <h3>john deo</h3>
+            <span>satisfied client</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, fugiat!</p>
+            <div className="stars">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+          <div className="box">
+            <img src="image/pic-3.png" alt="Client 3" />
+            <h3>john deo</h3>
+            <span>satisfied client</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, fugiat!</p>
+            <div className="stars">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="blogs" id="blogs">
+        <h1 className="heading">our <span>blogs</span></h1>
+        <div className="box-container">
+          <div className="box">
+            <div className="image">
+              <img src="image/blog-1.jpg" alt="Blog 1" />
+            </div>
+            <div className="content">
+              <div className="icon">
+                <a href="#"><i className="fas fa-calendar"></i> 21st may, 2021</a>
+                <a href="#"><i className="fas fa-user"></i> by admin</a>
+              </div>
+              <h3>blog title goes here</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+              <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+            </div>
+          </div>
+          <div className="box">
+            <div className="image">
+              <img src="image/blog-2.jpg" alt="Blog 2" />
+            </div>
+            <div className="content">
+              <div className="icon">
+                <a href="#"><i className="fas fa-calendar"></i> 21st may, 2021</a>
+                <a href="#"><i className="fas fa-user"></i> by admin</a>
+              </div>
+              <h3>blog title goes here</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+              <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+            </div>
+          </div>
+          <div className="box">
+            <div className="image">
+              <img src="image/blog-3.jpg" alt="Blog 3" />
+            </div>
+            <div className="content">
+              <div className="icon">
+                <a href="#"><i className="fas fa-calendar"></i> 21st may, 2021</a>
+                <a href="#"><i className="fas fa-user"></i> by admin</a>
+              </div>
+              <h3>blog title goes here</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+              <a href="#" className="btn">learn more <span className="fas fa-chevron-right"></span></a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="footer">
+  <div className="box-container">
+    <div className="box">
+      <h3>quick links</h3>
+      <a href="#"><i className="fas fa-chevron-right"></i> home</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> service</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> about</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> doctors</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> book</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> review</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> blogs</a>
+    </div>
+    <div className="box">
+      <h3>our services</h3>
+      <a href="#"><i className="fas fa-chevron-right"></i> dental care</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> massage therapy</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> cardiology</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> diagnosis</a>
+      <a href="#"><i className="fas fa-chevron-right"></i> ambulance service</a>
+    </div>
+    <div className="box">
+      <h3>contact info</h3>
+      <a href="tel:+201027523303"><i className="fas fa-phone"></i> +2-010-27-523303</a>
+      <a href="tel:+201121742287"><i className="fas fa-phone"></i> +2-011-21-742287</a>
+      <a href="mailto:abelrhman1562004@gmail.com"><i className="fas fa-envelope"></i> abelrhman1562004@gmail.com</a>
+      <a href="skype:abelrhman1562004"><i className="fas fa-envelope"></i> abelrhman1562004@skype.com</a>
+      <a href="#"><i className="fas fa-map-marker-alt"></i> el fayuom, elmaedany, 3300123</a>
+    </div>
+    <div className="box">
+      <h3>follow us</h3>
+      <a href="#"><i className="fab fa-facebook-f"></i> facebook</a>
+      <a href="#"><i className="fab fa-twitter"></i> twitter</a>
+      <a href="#"><i className="fab fa-instagram"></i> instagram</a>
+      <a href="#"><i className="fab fa-linkedin"></i> linkedin</a>
+      <a href="#"><i className="fab fa-pinterest"></i> pinterest</a>
+    </div>
+  </div>
+  <div className="credit">created &copy; by <span>abdelrhman ismail</span> | all rights reserved</div>
+</section>
+        </div>
+    )
+}
